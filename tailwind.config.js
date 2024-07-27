3c7183dae46107d9448eb09e541d52183f135624
Nadav Spiegelman <me@nadav.name>
2024-07-27 13:17:36 -0400

    Tweak table styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,8 @@ export default {
 					'--tw-prose-quotes': theme('colors.greenlake[800]'),
 					'--tw-prose-quote-borders': theme('colors.greenlake[600]'),
 					'--tw-prose-captions': theme('colors.greenlake[800]'),
+					'--tw-prose-th-borders': theme('colors.tumbleweed[500]'),
+					'--tw-prose-td-borders': theme('colors.purple.dark'),
 					'--tw-prose-invert-body': theme('colors.purple.light'),
 					'--tw-prose-invert-bullets': theme('colors.purple.light'),
 					'--tw-prose-invert-quotes': theme('colors.purple.light'),
