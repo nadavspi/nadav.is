7d60dcea2a05700336a78b02f0f61cbd1ddb31d7
Nadav Spiegelman <me@nadav.name>
2025-03-24 22:23:35 -0400

    Add vercel adapter
    https://docs.astro.build/en/guides/integrations-guide/vercel/

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,18 @@ import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind({
     applyBaseStyles: false
   }), preact({ compat: true })],
+
   prefetch: {
     prefetchAll: true
   },
+
   site: "https://nadav.is",
+  adapter: vercel(),
 });
