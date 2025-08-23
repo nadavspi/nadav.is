270c04cf9cb67532f8bebb3862e536d20b38a22d
Nadav Spiegelman <me@nadav.name>
2025-08-23 16:53:31 -0400

    Remove tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel";
@@ -9,9 +8,6 @@ import vercel from "@astrojs/vercel";
 export default defineConfig({
   integrations: [
     mdx(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
     preact({ compat: true }),
   ],
 
